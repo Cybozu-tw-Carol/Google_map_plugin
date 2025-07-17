98ead222d2d61c288c51b3fb5921c65498b01a50
Carol <h003344@cybozu.onmicrosoft.com>
2025-07-17 09:27:46 +0800

    feature: add setError() to control error message

--- src/js/customize.js
+++ src/js/customize.js
@@ -16,9 +16,10 @@
   kintone.events.on('app.record.detail.show', event => {
     const { record } = event
     const map = kintone.app.record.getSpaceElement(mapField)
+
+    if (!map) return setError(`找不到設定的空白欄位 ${mapField}，請確定欄位是否存在`, event);
     if (!record[latField] || !record[lngField]) {
-      event.error = '設定的緯經度欄位不存在於此應用，請檢查設定'
-      return event
+      return setError('設定的緯經度欄位不存在於此應用，請檢查設定', event);
     }
     let lat = record[latField].value
     let lng = record[lngField].value
@@ -47,16 +48,11 @@
     })
     if (missingFields.length > 0) {
       const missingField = missingFields.map(([, value]) => value).join('、')
-      event.error = `缺少以下欄位資訊：${missingField}，請檢查外掛欄位設定`
-      return event
+      return setError(`缺少以下欄位資訊：${missingField}，請檢查外掛欄位設定`, event);
     }
 
     const address = record[addressField].value
-    if (!address) {
-      event.error = '地址欄位為空，請先填入地址'
-      console.warn('地址欄位為空，請先填入地址')
-      return event;
-    }
+    if (!address) return setError('地址欄位為空，請先填入地址', event);
 
     const fullURL = `${GEOCODE_API}?address=${encodeURIComponent(address)}`
     try {
@@ -64,22 +60,20 @@
       const body = JSON.parse(res[0])
 
       if (body.status !== 'OK' || body.results.length === 0) {
-        const message = `地址查詢失敗：Google Maps API 回傳狀態 ${body.status}`
-        console.warn(message, body);
-        event.error = message
-        return event
+        return setError(`地址查詢失敗：Google Maps API 回傳狀態 ${body.status}`, event);
       }
       record[latField].value = body.results[0].geometry.location.lat
       record[lngField].value = body.results[0].geometry.location.lng
 
       return event
     } catch (error) {
-      const msg = error.message
-      event.error = `地址查詢時發生錯誤：${msg}`;
-      console.warn('Geocode Error:', error);
-      return event;
+      return setError(`地址查詢時發生錯誤：${error.message}`, event);
     }
 
   })
-
+function setError(message, event) {
+  event.error = message;
+  console.warn(message);
+  return event;
+}
 })(kintone.$PLUGIN_ID)
